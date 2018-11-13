937ccd78947e4589ba3cae877c2a3f80c3a9f4a3
Nexis Sis <nexis.sis@gmail.com>
2018-11-13 15:15:33 +0300

    added mobile and small refactor

--- src/blocks/Header/Nav/Nav.jsx
+++ src/blocks/Header/Nav/Nav.jsx
@@ -8,25 +8,33 @@ const cnNav = cn('Nav');
 class Nav extends React.Component {
     render() {
         return (
-            <div className={cnNav() + " flex flex-jc-sb"}>
-                <a className={cnNav('Item')} href="/">
-                    <span className={cnNav('Text')}>События</span>
-                </a>
-                <a className={cnNav('Item')} href="/">
-                    <span className={cnNav('Text')}>Сводка</span>
-                </a>
-                <a className={cnNav('Item')} href="/">
-                    <span className={cnNav('Text')}>Устройства</span>
-                </a>
-                <a className={cnNav('Item')} href="/">
-                    <span className={cnNav('Text')}>Сценарии</span>
-                </a>
-                <a className={cnNav('Item')} href="/">
-                    <span className={cnNav('Text')}>Сенсорныый ввод</span>
-                </a>
-                <a className={cnNav('Item')} href="/">
-                    <span className={cnNav('Text')}>Камеры наблюдения</span>
-                </a>
+            <div>
+                <div class="Nav-Mobile">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+
+                <div className={cnNav() + " flex flex-jc-sb"}>
+                    <a className={cnNav('Item')} href="/">
+                        <span className={cnNav('Text')}>События</span>
+                    </a>
+                    <a className={cnNav('Item')} href="/">
+                        <span className={cnNav('Text')}>Сводка</span>
+                    </a>
+                    <a className={cnNav('Item')} href="/">
+                        <span className={cnNav('Text')}>Устройства</span>
+                    </a>
+                    <a className={cnNav('Item')} href="/">
+                        <span className={cnNav('Text')}>Сценарии</span>
+                    </a>
+                    <a className={cnNav('Item')} href="/">
+                        <span className={cnNav('Text')}>Сенсорныый ввод</span>
+                    </a>
+                    <a className={cnNav('Item')} href="/">
+                        <span className={cnNav('Text')}>Камеры наблюдения</span>
+                    </a>
+                </div>
             </div>
         );
     }
