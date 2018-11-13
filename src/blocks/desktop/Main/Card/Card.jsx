c03470f7acd9f3b8783fd7d95f50d40198fff192
Nexis Sis <nexis.sis@gmail.com>
2018-11-13 14:34:52 +0300

    finished cards

--- src/blocks/desktop/Main/Card/Card.jsx
+++ src/blocks/desktop/Main/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { cn } from '@bem-react/classname';
 import './Card.scss';
+import Player from './Player/Player';
 
 const cnCard = cn('Card');
 
@@ -8,30 +9,55 @@ class Card extends React.Component {
 
     render() {
         //default value
-        let sizeCardCss = "grid-c-4";
-        let flexCardCss = "flex flex-jc-sb-m flex-fd-col-d flex-al-fs-d";
+        let cardCss = "grid-c-4";
+        let flexCardCss = "flex flex-jc-sb";
         if(this.props.size === "big"){
-            sizeCardCss = "grid-c-8 grid-r-3";
+            cardCss = "grid-c-8 grid-r-3";
             flexCardCss = "flex flex-jc-sb";
-        }else if(this.props.size === "small"){
-            sizeCardCss = "grid-c-4";
+        }else if(this.props.size === 'middle'){
+            cardCss = "grid-c-6";
+        } if(this.props.size === "small"){
+            cardCss = "grid-c-4";
             flexCardCss = "flex flex-jc-sb-m flex-fd-col-d flex-al-fs-d";
         }
+        let indent = 'Card_m-t';
+        let colorCss = '';
+        let paddingCss = '';
+        let CardCssForRed = '';
+        if(this.props.red && this.props.red !== 'false'){
+            cardCss+= ' no-padding Card_Red';
+            colorCss = 'color_white';
+            indent = paddingCss = 'p-20-20-0';
+            CardCssForRed = cnCard();
+        }
         return (
-            <div className={ cnCard() + " " + sizeCardCss}>
+            <div className={ cnCard() + " " + cardCss}>
                 <div className="icon-close"></div>
-                <div className={"Card_m-t flex"}>
-                    <div className={cnCard("Icon") + " " + this.props.iconName}></div>
-                    <div className={cnCard("Title")}>{this.props.title}</div>
+                <div className={"flex" + ' ' + indent}>
+                    <div className={cnCard("Icon") + " " + this.props.iconName + " " + colorCss}></div>
+                    <div className={cnCard("Title") + " " + colorCss}>{this.props.title}</div>
                 </div>
-                <div className={flexCardCss}>
-                    <div className={cnCard("Text")}>{this.props.sensorName}</div>
-                    <div className={cnCard("Text")}>{this.props.time}</div>
+                <div className={flexCardCss + ' ' + paddingCss}>
+                    <div className={cnCard("Text") + " " + colorCss}>{this.props.sensorName}</div>
+                    <div className={cnCard("Text") + " " + colorCss}>{this.props.time}</div>
                 </div>
-                { this.props.description ? <div className={cnCard("Description") + " mt-24"}> {this.props.description} </div> : ' ' }
+                <div className={CardCssForRed + " mt-24"}>
+                    { this.props.description ? <div className={cnCard("Description")}> {this.props.description} </div> : ' ' }
+                    { this.props.description2 ? <div className={cnCard("Description-2")}> {this.props.description2} </div> : ' ' }
+                    { this.props.imageName ? <img className={cnCard("Image") + " mt-54"} src={"img/" + this.props.imageName}/> : '' }
+                    { this.props.player && this.props.player !== 'false' ? <Player/>  : '' }
 
-                { this.props.imageName ? <img className={cnCard("Image") + " mt-54"} src={"img/" + this.props.imageName} alt="profffesional graph"/> : '' }
+                    { this.props.buttons && this.props.buttons !== 'false' ?
+                    <div class="flex mt-48 flex-jc-sb-m">
+                        <button className={cnCard('Button',{'Yellow':true})}>Да</button>
+                        <button className={cnCard('Button') + " ml-16"}>Нет</button>
+                    </div> : ''}
 
+                    <div class="flex">
+                        { this.props.temperature ? <div class="card__text color_black  mt-58">Температура: <span class="text_strong">{this.props.temperature}</span></div> : ''}
+                        { this.props.humidity ? <div class="card__text ml-54 color_black  mt-58">Влажность: <span class="text_strong">{this.props.humidity}</span></div> : ''}
+                    </div>
+                </div>
                 <div className="icon-next"></div>
             </div>
         );
--- src/blocks/desktop/Main/Main.jsx
+++ src/blocks/desktop/Main/Main.jsx
@@ -37,7 +37,7 @@ class Header extends React.Component {
                     <Card
                         size="small"
                         iconName="icon-everyday"
-                        title="Новый юзер"
+                        title="Новый пользователь"
                         sensorName="Роутер"
                         time="19:00, Сегодня"
                     />
@@ -48,8 +48,70 @@ class Header extends React.Component {
                         sensorName="Сенсор микроклимата"
                         time="18:30, Сегодня"
                         description2="Установлен климатический режим «Фиджи»"
+                        temperature="24C"
+                        humidity="80%"
                     />
-
+                    <Card
+                        size="middle"
+                        iconName="icon-everyday"
+                        title="Невозможно включить кондиционер"
+                        sensorName="Кондиционер"
+                        time="18:21, Сегодня"
+                        description2="В комнате открыто окно, закройте его и повторите попытку"
+                        red="true"
+                    />
+                    <Card
+                        size="middle"
+                        iconName="icon-everyday"
+                        title="Музыка включена"
+                        sensorName="Яндекс.Станция"
+                        time="18:16, Сегодня"
+                        description2="Сейчас проигрывается:"
+                        player="true"
+                    />
+                    <Card
+                        size="middle"
+                        iconName="icon-everyday"
+                        title="Заканчивается молоко"
+                        sensorName="Холодильник"
+                        time="17:23, Сегодня"
+                        description2="Кажется, в холодильнике заканчивается молоко. Вы хотите добавить его в список покупок?"
+                        buttons="true"
+                    />
+                    <Card
+                        size="small"
+                        iconName="icon-everyday"
+                        title="Зарядка завершена"
+                        sensorName="Оконный сенсор"
+                        time="16:22, Сегодня"
+                        description2="Ура! Устройство «Оконный сенсор» снова в строю!"
+                    />
+                    <Card
+                        size="small"
+                        iconName="icon-everyday"
+                        title="Зарядка завершена"
+                        sensorName="Оконный сенсор"
+                        time="16:22, Сегодня"
+                        description2="Ура! Устройство «Оконный сенсор» снова в строю!"
+                    />
+                    <Card
+                        size="small"
+                        iconName="icon-everyday"
+                        title="Зарядка завершена"
+                        sensorName="Оконный сенсор"
+                        time="16:22, Сегодня"
+                        description2="Ура! Устройство «Оконный сенсор» снова в строю!"
+                    />
+                    <Card
+                        size="big"
+                        iconName="icon-everyday"
+                        title="Пылесос застрял"
+                        sensorName="Сенсор движения"
+                        time="16:17, Сегодня"
+                        description="Робопылесос не смог сменить свое местоположение в течение последних 3 минут. Похоже, ему нужна помощь."
+                        imageName="card-last-m.png"
+                        red="true"
+                    />
 
                 </div>
             </div>
